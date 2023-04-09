84ca3b38cc440549465099afa68a45d51b11fe1d
amelie-pf <amelie.pfaender@gmx.de>
2023-04-08 18:43:33 -0700

    new nav bar

--- javascript/animation.js
+++ javascript/animation.js
@@ -33,3 +33,19 @@ function goBack()
   {
   window.history.back()
   }
+
+
+
+/* neuer code für nav bar mit burger menue */
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
